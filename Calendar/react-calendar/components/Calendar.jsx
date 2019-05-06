cf319999a1b022048790536bff8a0fac1eaa0567
Bertram, Travis <travis.bertram@siemens.com>
2019-05-06 10:34:08 -0500

    first commit

---
+++ Calendar/react-calendar/components/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import dateFns from "dateFns";
+
+class Calendar extends React.Component {
+    render () {
+        return (
+          <div>
+              <div>Header</div>
+              <div>Days</div>
+              <div>Cells</div>
+          </div>  
+        );
+    }
+}
---
+++ Calendar/react-calendar/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import Calendar from "./components/Calendar";
+import './App.css';
+
+class App extends React.Component {
+  render(){
+  return (
+    <div className="App">
+      <header>
+        <div id="logo">
+        <span className="icon">date_range</span>
+        <span>
+          react<b>Calendar</b>
+        </span>
+        </div>
+      </header>
+      <main>
+        <Calendar />
+      </main>
+    </div>
+  );
+  }
+}
+
+export default App;
